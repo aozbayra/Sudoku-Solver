9db40c9a8765759e354e80bac350367810b9592f
Alihan Ozbayrak <aozbayra@purdue.edu>
2014-10-28 20:52:05 -0400

    Fixed the candidates method and testCandidates
    Give it another shot for testnakedSingles but make sure you understand
    the method first

--- SudokuTest.java
+++ SudokuTest.java
@@ -7,8 +7,7 @@ import static org.junit.Assert.*;
  * Every method starting with the word "test" will be called when running
  * the test with JUnit.
  */
-public class SudokuTest extends TestCase {
-        Sudoku s = new Sudoku();       
+public class SudokuTest extends TestCase {      
     /**
      * A test method.
      * (Replace "X" with a name describing the test.  You may write as
@@ -26,55 +25,20 @@ public class SudokuTest extends TestCase {
                 counter++;
             }
         }
-        boolean[] array = s.candidates(4,5);
-        boolean[] testArray = {false, false, false, true, true, false, false, false, false, false};
+        Sudoku s = new Sudoku(matrix);
+        boolean[] array = s.candidates(4,5);  // 3 and 4 should come out
+        boolean[] testArray = {false, false, true, true, false, false, false, false, false};
         assertTrue(Arrays.equals(testArray, array));
     }
     
     public void testNakedSingles() {
-        int[] row = new int[9];
-        String r = "024587693";
-        int count = 0;
-        for (int t = 0; t < row.length; t++) {
-            row[t] = r.charAt(count) - 48;
-            count++;
-            if (row[t] == 0) {
-                boolean d = s.nakedSingles();
-                assertTrue(d);
-            }
-        }
         
-        int[][] column = new int[1][9];
-        String c = "410782693";
-        int u = 0;
-        for (int m = 0; m < column[1].length; m++){
-            column[1][m] = c.charAt(u) - 48;
-            if (column[1][m] == 0) {
-                boolean x = s.nakedSingles();
-                assertTrue(x);
-            }
-        }
-        
-        int[][] mat = new int[3][3];
-        String test = "246813570";
-        int v = 0;
-        for (int z = 0; z < mat.length; z++) {
-            for (int q = 0; q < mat[z].length; q++) {
-                mat[z][q] = test.charAt(v) - 48;
-                if (mat[z][q] == 0) {
-                    boolean f = s.nakedSingles();
-                    assertTrue(f);
-                }
-            }
-        }
     }
     
     public void testHiddenSingles() {
     }
     
     public void testIsSolved() {
-        boolean m = s.isSolved();
-        assertFalse(m);
     }
     
     public void testSolve() {
